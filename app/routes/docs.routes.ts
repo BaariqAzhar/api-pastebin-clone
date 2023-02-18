7c5e3f21a32edd045d03d7c565f02639360aeec3
baariq <baariq.fairuuz@dataon.com>
2023-02-18 14:38:52 +0700

    add readAll controller

--- app/routes/docs.routes.ts
+++ app/routes/docs.routes.ts
@@ -1,12 +1,16 @@
 import express, { Express } from 'express';
 
-import createController from '../controllers/create.controller';
 import authenticateUserHelper from '../helper/authenticateUser.helper';
+
+import createController from '../controllers/create.controller';
+import readAllController from '../controllers/readAll.controller';
 
 const docsRoutes = (app: Express) => {
     let router = express.Router();
 
     router.post('/create', authenticateUserHelper, createController);
+
+    router.post('/readAll', authenticateUserHelper, readAllController);
 
     app.use('/docs', router);
 };
